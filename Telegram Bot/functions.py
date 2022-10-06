74edf4097e647451491641782477dc22188801e8
stas.pereverzev <stas.pereverzev@internet.ru>
2022-10-06 19:59:51 +0300

    commit

---
+++ Telegram Bot/functions.py
@@ -0,0 +1,14 @@
+from pyowm import OWM
+
+def weather(city):
+    owm = OWM('dbf1c716cecbf43263218e4e0505968c') 
+
+       
+    mgr = owm.weather_manager()
+    observation = mgr.weather_at_place(city)
+    w = observation.weather
+    t = w.temperature('celsius')['temp']
+    w = w.wind()['speed'] 
+ 
+
+    return (f'В городе {city} сейчас {round(t)} C, cкорость ветра: {w} м/с.')
---
+++ Telegram Bot/main.py
@@ -0,0 +1,22 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from bot_commands import *
+
+
+
+
+app = ApplicationBuilder().token("5786521972:AAGTdVIhx6C5cWsmYIsxJEnRouXmlq6IZq4").build()
+
+app.add_handler(CommandHandler("hi", hi_command))
+app.add_handler(CommandHandler("time", time_command))
+app.add_handler(CommandHandler("help", help_command))
+app.add_handler(CommandHandler("sum",sum_command))
+app.add_handler(CommandHandler("mult",mult_command))
+app.add_handler(CommandHandler("div",div_command))
+app.add_handler(CommandHandler("sub",sub_command))
+app.add_handler(CommandHandler("calc",calc_command))
+app.add_handler(CommandHandler("temp",weather))
+
+
+print('server start')
+app.run_polling()
